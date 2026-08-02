3290aa3da868c3794315eb25e0638314cfa14178
Preeti Pragyan Mishra <preetipmishra2@gmail.com>
2026-08-02 10:54:00 +0530

    Refactor binary search for finding partition point

--- LAB1/Q5.c
+++ LAB1/Q5.c
@@ -1,49 +1,25 @@
 #include <stdio.h>
-//BS to find the index of the first occurrence of 1  in a sorted binary array containing a run of 0s followed by 1s.
-int part(int A[], int n) {
-    int low = 0;
-    int high = n - 1;
-    int idx = -1;
 
+//bs to find the transition point from 0 to 1
+int find_partition(int A[], int n) {
+    int low = 0, high = n - 1, ans = -1;
     while (low <= high) {
         int mid = low + (high - low) / 2;
-
         if (A[mid] == 1) {
-            //potential partition point found search left half for an earlier '1'
-            idx = mid;
+            ans = mid;     //check left
             high = mid - 1;
         } else {
-            // A[mid] == 0 means go to search right half
-            low = mid + 1;
+            low = mid + 1; //check right, otherwise
         }
     }
-
-    return idx;
+    return ans; 
 }
 
 int main() {
-    //array where 0s followed by 1s
-    int A[] = {0, 0, 0, 0, 0, 1, 1, 1, 1};
+    int A[] = {0, 0, 0, 0, 1, 1, 1};
     int n = sizeof(A) / sizeof(A[0]);
-
-    printf("Array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", A[i]);
-    }
-    printf("\n");
-
-    int index = part(A, n);
-
-    if (index != -1) {
-        printf("Partition point found at Index: %d\n", index);
-        if (index > 0) {
-            printf("Elements: A[%d] = %d -> A[%d] = %d\n", index - 1, A[index - 1], index, A[index]);
-        } else {
-            printf("The array consists of only 1's.\n");
-        }
-    } else {
-        printf("no 1's found in the array\n");
-    }
-
+    
+    int index = find_partition(A, n);
+    printf("Partition point (first 1) is at index: %d\n", index);
     return 0;
 }
